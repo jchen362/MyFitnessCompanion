41b95fef138b7b1de1017314926936a83aaa485c
Jianwei Chen <98350779+jchen362@users.noreply.github.com>
2023-07-27 11:16:21 -0400

    removed session expiration for testing purposes and added imgs

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -10,6 +10,9 @@ import TextField from '@mui/material/TextField';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import IconButton from '@mui/material/IconButton';
 import 'chartjs-adapter-date-fns';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import exerciseImg from "./imgs/girl-doing-yoga.svg";
 
 import {
     LineElement,
@@ -47,6 +50,8 @@ class Homepage extends React.Component {
                 },
             },
             addWeight: 0,
+            name: "PlaceHolder",
+            exerciseData: [],
             
         };
 
@@ -70,6 +75,7 @@ class Homepage extends React.Component {
 
         const data = await response.json();
         if (data.user === false) { //session expired
+            console.log("session expired");
             p.navigation("/");
         }
         
@@ -131,15 +137,47 @@ class Homepage extends React.Component {
         }});
     }
 
+    async downloadExercises(p) {
+        const response = await fetch('https://api.api-ninjas.com/v1/exercises?muscle=', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Api-Key': 'gXjBVRIQhqEqD6hjUFUj+A==8Pe2On2DBqTw5Jep',
+            },
+        });
+
+        const exerciseData = await response.json();
+        this.setState({exerciseData: exerciseData});
+    }
+
+    async getName(p) {
+        let token = p.location.state.token;
+        const response = await fetch("http://localhost:3001/api/getName", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                token,
+            }),
+        });
+
+        const data = await response.json();
+        if (data.name != false) {
+            this.setState({name: data.name});
+        }
+    }
+
     async componentDidMount() {
         await this.downloadData(this.props);
-        this.interval = setInterval(this.verifySession, 60000, this.props);
+        await this.getName(this.props);
+        await this.downloadExercises(this.props);
+        //this.interval = setInterval(this.verifySession, 60000, this.props);
     }
 
     componentWillUnmount() {
         //TODO: call server to end session
-
-        clearInterval(this.interval);
+        //clearInterval(this.interval);
     }
     render() {
 
@@ -154,28 +192,48 @@ class Homepage extends React.Component {
                 {/**Container for rest of things */}
                 <div style = {{width: "100%", height: "740px", flexDirection: "row", display: "flex"}}>
                     {/**Left Side */}
-                    <div style = {{width: "45%", height: "100%", borderWidth: "1px", borderStyle: "solid", borderColor: "black", flexDirection: "column", display: "flex", justifyItems: "center"}}>
-                        <div style = {{width: "100%", height: "0.5%"}}></div>
-                        <div style = {{wdith: "100%", height: "40%", borderWidth: "1px", borderStyle: "solid", borderColor: "black", borderRadius: "20px"}}>
-                            <div style = {{width: "100%", height: "100%", borderWidth: "1px", borderStyle: "solid", borderColor: "black", borderRadius: "20px", display: "flex", justifyContent: "center"}}>
-                                <Line data = {this.state.weightData} options = {this.state.options}></Line>
+                    <div style = {{width: "45%", height: "100%", flexDirection: "column", display: "flex", justifyItems: "center"}}>
+                        <div style = {{width: "100%", height: "15px"}}></div>
+                        <Paper elevation = {5} sx = {{borderRadius: "20px"}}>
+                            <div style = {{wdith: "100%", height: "40%"}}>
+                                <div style = {{width: "100%", height: "300px", display: "flex", justifyContent: "center"}}>
+                                    <Line data = {this.state.weightData} options = {this.state.options}></Line>
+                                </div>
+                                <div style = {{width: "100%", height: "40px", display: "flex", flexDirection: "row", marginTop: "10px", marginLeft: "10px"}}>
+                                    <TextField variant = "outlined" label = "Enter Weight" size = "small" sx = {{width: "120px", height: "15px"}} value = {this.state.addWeight} onChange = {(e)=> {
+                                        this.setState({addWeight: e.target.value})
+                                    }}/>
+                                    <IconButton onClick = {() => {
+                                        this.uploadData(this.props);
+                                    }}>
+                                        <AddCircleIcon/>
+                                    </IconButton>
+                                </div>
                             </div>
-                            <div style = {{width: "100%", height: "40px", display: "flex", flexDirection: "row", marginTop: "10px", marginLeft: "10px"}}>
-                                <TextField variant = "outlined" label = "Enter Weight" size = "small" sx = {{width: "120px", height: "15px"}} value = {this.state.addWeight} onChange = {(e)=> {
-                                    this.setState({addWeight: e.target.value})
-                                }}/>
-                                <IconButton onClick = {() => {
-                                    this.uploadData(this.props);
-                                }}>
-                                    <AddCircleIcon/>
-                                </IconButton>
-                            </div>
-                        </div>
+                        </Paper>
                     </div>
                     <div style = {{width: "0.5%", height: "100%"}}></div>
                     {/**Right side */}
-                    <div style = {{width: "54.5%", height: "100%", borderWidth: "1px", borderStyle: "solid", borderColor: "black"}}>
-                        Right Side
+                    <div style = {{width: "54.5%", height: "100%"}}>
+                        <div style = {{width: "100%", height: "15px"}}></div>
+                        <Paper elevation = {5} sx = {{borderRadius: "20px"}}>
+                            <div style = {{width: "100%", height: "40%", display: "flex", flexDirection: "column", backgroundImage: `url(${exerciseImg})`, backgroundRepeat: "no-repeat", backgroundPosition: "right 20px bottom 30px"}}>
+                                <p style = {{fontWeight: "bold", paddingLeft: "10px", fontSize: "30px"}}>
+                                    {"Welcome, " + this.state.name}
+                                </p>
+                                <p style = {{paddingLeft: "10px", fontSize: "18px"}}>
+                                    Remember to track all your workouts, weight, and nutrition to see your daily progress towards your fitness goals!
+                                </p>
+                            </div>
+                        </Paper>
+                        <div style = {{width: "100%", height: "15px"}}></div>
+                        <Paper elevation = {5} sx = {{borderRadius: "20px"}}>
+                            <div style = {{width: "100%", height: "40%", display: "flex", flexDirection: "column"}}>
+                                <p style = {{fontWeight: "bold", paddingLeft: "10px", fontSize: "30px"}}>
+                                    Workout of the Day
+                                </p>
+                            </div>
+                        </Paper>
                     </div>
                 </div>
             </motion.div>
